1906fc15c2acf1e11dc8a0e73ae1437b2617117f
alejandrodk <ottoabarriosp@hotmail.com>
2021-08-28 12:26:30 -0300

    Create Tools Bar

---
+++ src/components/MyTools/MyTools.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+
+import Controller from "../../api";
+import { Container, Tool, ToolImg, Quantity, Space } from "./MyToolsStyles";
+
+const HeaderComp = ({ token }) => {
+  const [tools, setTools] = useState(0);
+
+  useEffect(() => {
+    if (!tools) {
+      (async function () {
+        const controller = new Controller(token);
+        const { data } = await controller.myTools();
+
+        if (data) {
+          setTools(data);
+        }
+      })();
+    }
+  }, [tools]);
+
+  function getImage(type) {
+    switch (type) {
+      case "Small Pot":
+        return "small_pot.png";
+      case "Big Pot":
+        return "big_pot.png";
+      case "Water":
+        return "water.png";
+      case "Scarecrow":
+        return "scare_crow.png";
+      case "Greenhouse":
+        return "green_house.png";
+      default:
+        return "favicon.svg";
+    }
+  }
+
+  return (
+    <Container>
+      {tools &&
+        tools.map((tool, ix) => (
+          <React.Fragment>
+            <Tool>
+              <ToolImg src={getImage(tool.name)} alt={tool.name} />
+              <Quantity>{tool.usages}</Quantity>
+            </Tool>
+            {ix < tools.length - 1 && <Space />}
+          </React.Fragment>
+        ))}
+    </Container>
+  );
+};
+
+export default HeaderComp;
---
+++ src/components/MyTools/MyToolsStyles.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: space-between;
+  background-color: #ffc745;
+  border: 2px solid #333;
+  box-shadow: 0 8px 0 rgb(0 0 0 / 20%), inset 0 -5px 0 #d6a638,
+    inset 0 5px 0 #ffd77a;
+  padding: 20px;
+  z-index: 3;
+  margin: 15px;
+  border-radius: 15px;
+`;
+
+export const Tool = styled.div`
+  position: relative;
+  background: #d6a638;
+  border-radius: 15px;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+  padding-right: 10px;
+`;
+
+export const ToolImg = styled.img`
+  width: 50px;
+`;
+
+export const Quantity = styled.div`
+  position: absolute;
+  background: #96bf4e;
+  top: 0;
+  right: -15%;
+  font-family: Luckiest Guy;
+  color: white;
+  padding: 5px;
+  border-radius: 10px;
+`;
+
+export const Space = styled.span`
+  width: 15px;
+`
