a9c84cd3eef7950ec71e4f260615b63c0e34fa7d
Alejandro Barrios <ottoabarriosp@hotmail.com>
2021-09-08 22:37:20 -0300

    Add notification support

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,5 +1,30 @@
+const audio = new Audio("/notif.mp3");
+
 export async function sleep(seconds) {
   return new Promise((res) => {
     setTimeout(res, seconds * 1000);
   });
+}
+
+export function getQueryParam(name) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(name);
+}
+
+export function checkAudio() {
+  return audio.muted;
+}
+
+export function setAudio(value) {
+  audio.muted = value;
+}
+
+export function soundNotification() {
+  const playPromise = audio.play();
+
+  if (playPromise) {
+    playPromise
+      .then((_) => {})
+      .catch(console.error);
+  }
 }
